9ba1ad8579e4c2a70174b8fbf7f818574a19f157
arturo <arturovazquezpaumard@gmail.com>
2025-05-22 12:06:36 +0200

    He pintado el formulario de registro y dado estilos css

--- client/src/pages/PublicPages/Register/Register.jsx
+++ client/src/pages/PublicPages/Register/Register.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import './register.css';
 
 const Register = () => {
   return (
-    <div>
-      Register
-    </div>
-  )
-}
+    <section>
+      <Container>
+        <Row className="pt-5 mt-5 d-flex align-items-center text-center form">
+          <Col md={12} lg={12}>
+            <h1>No hace falta brújula</h1>
+            <p>
+              Sigue el formulario y acabarás donde se duerme mejor -en plena
+              naturaleza
+            </p>
+
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Label htmlFor="disabledTextInput">
+                  Disabled input
+                </Form.Label>
+                <Form.Control
+                  id="disabledTextInput"
+                  placeholder="Disabled input"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label htmlFor="disabledTextInput">
+                  Disabled input
+                </Form.Label>
+                <Form.Control
+                  id="disabledTextInput"
+                  placeholder="Disabled input"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label htmlFor="disabledTextInput">
+                  Disabled input
+                </Form.Label>
+                <Form.Control
+                  id="disabledTextInput"
+                  placeholder="Disabled input"
+                />
+              </Form.Group>
+
+              <Button type="submit">Submit</Button>
+
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
 
 export default Register;
