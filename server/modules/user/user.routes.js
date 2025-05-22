d04214edbac9c2500081984c1304e47effe3785d
eli <k4in16@gmail.com>
2025-05-22 09:15:35 +0200

    cositas

--- server/modules/user/user.routes.js
+++ server/modules/user/user.routes.js
@@ -3,10 +3,8 @@ import UserControllers from './user.controllers.js';
 
 const router = express.Router();
 
-
 router.put('/editUser/:id', UserControllers.editUserById);
 
 router.post('/contact', UserControllers.sendEmail);
-
 
 export default router;
