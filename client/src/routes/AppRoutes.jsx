2e3e12a16b0b0434513b805d187be707d01deed5
eli <k4in16@gmail.com>
2025-05-23 17:36:03 +0200

    funcionalidades de usuarios en la parte de administrador

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -6,43 +6,49 @@ import { UserLayout } from '../layouts/UserLayout';
 import { AdminLayout } from '../layouts/AdminLayout';
 import { PrivateRoutes } from './PrivateRoutes';
 
-
 // componentes publics
 const Home = lazy(() => import('../pages/publicPages/Home/Home'));
-const CamperNature = lazy(() => import("../pages/PublicPages/CamperNature/CamperNature"));
-const Tarifas = lazy(() => import("../pages/PublicPages/Tarifas/Tarifas"));
-const Entorno = lazy(() => import("../pages/PublicPages/Entorno/Entorno"));
-const Reservas = lazy(() => import("../pages/PublicPages/Reservas/Reservas"));
+const CamperNature = lazy(() =>
+  import('../pages/PublicPages/CamperNature/CamperNature')
+);
+const Tarifas = lazy(() => import('../pages/PublicPages/Tarifas/Tarifas'));
+const Entorno = lazy(() => import('../pages/PublicPages/Entorno/Entorno'));
+const Reservas = lazy(() => import('../pages/PublicPages/Reservas/Reservas'));
 const Login = lazy(() => import('../pages/PublicPages/Login/Login'));
 const Register = lazy(() => import('../pages/PublicPages/Register/Register'));
 const Contact = lazy(() => import('../pages/PublicPages/Contact/Contact'));
 
 //componentes user
-const UserProfile = lazy(() => import('../pages/UserPages/UserProfile/UserProfile'));
+const UserProfile = lazy(() =>
+  import('../pages/UserPages/UserProfile/UserProfile')
+);
 const EditUser = lazy(() => import('../pages/UserPages/EditUser/EditUser'));
 
 //componentes admin
-const EditService = lazy(() => import('../pages/AdminPages/EditService/EditService'));
-const CreateService = lazy(()=> import ('../pages/AdminPages/CreateService/CreateService'))
-
+const EditService = lazy(() =>
+  import('../pages/AdminPages/EditService/EditService')
+);
+const CreateService = lazy(() =>
+  import('../pages/AdminPages/CreateService/CreateService')
+);
+const UserList = lazy(() => import('../pages/AdminPages/UserList/UserList'));
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Suspense fallback={<h1>Marina cagando...</h1>}>
         <Routes>
-         
           {/* RUTAS PÚBLICAS */}
-           <Route element={<PublicRoutes />}>
-              <Route element={<PublicLayout />}>
-                  <Route path="/register" element={<Register />} />
-                  <Route path="/" element={<Home />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/campernature" element={<CamperNature/>} />
-                  <Route path ="/tarifas" element={<Tarifas/>} />
-                  <Route path ="/entorno" element={<Entorno/>} />
-                  <Route path ="/reservas" element={<Reservas/>} />
-                  <Route path="/contact" element={<Contact />} />     
+          <Route element={<PublicRoutes />}>
+            <Route element={<PublicLayout />}>
+              <Route path="/register" element={<Register />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/campernature" element={<CamperNature />} />
+              <Route path="/tarifas" element={<Tarifas />} />
+              <Route path="/entorno" element={<Entorno />} />
+              <Route path="/reservas" element={<Reservas />} />
+              <Route path="/contact" element={<Contact />} />
             </Route>
           </Route>
           <Route element={<PrivateRoutes />}>
@@ -55,14 +61,12 @@ export const AppRoutes = () => {
               {/* AQUI VIENEN LAS COSAS DE LA COSAS DE LOS USERS*/}
             </Route>
           </Route>
-
           {/* RUTAS ADMIN SIN PROTECCIÓN */}
           <Route element={<AdminLayout />}>
-            <Route path='/admin/service' element={<CreateService/>}/>
+            <Route path="/admin/service" element={<CreateService />} />
             {/* Agrega rutas de admin aquí si es necesario */}
             <Route path="/admin/editService/:id" element={<EditService />} />
-            
-           
+            <Route path="/admin/userList" element={<UserList />} />
           </Route>
         </Routes>
       </Suspense>
--- server/modules/admin/admin.controllers.js
+++ server/modules/admin/admin.controllers.js
@@ -1,49 +1,60 @@
 import userDal from '../user/user.dal.js';
-import adminDal from "./admin.dal.js"
-import dotenv from "dotenv"
+import adminDal from './admin.dal.js';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-class AdminControllers{
+class AdminControllers {
   /* EDITAR SERVICIO */
-  editService= async(req,res) =>{
-       let id = req.params.id
-       let data = {...req.body, service_id:id}
-    try{
-     await adminDal.editService(data, req.file) 
-     console.log("ID RECIBIDO por el ADMIN CONTROLLER", req.params.id)
-     console.log("BODY RECIBIDO por el ADMIN CONTROLLER", req.body);
-     
+  editService = async (req, res) => {
+    let id = req.params.id;
+    let data = { ...req.body, service_id: id };
+    try {
+      await adminDal.editService(data, req.file);
+      console.log('ID RECIBIDO por el ADMIN CONTROLLER', req.params.id);
+      console.log('BODY RECIBIDO por el ADMIN CONTROLLER', req.body);
 
+      res.status(200).json({ message: 'Servicio editado correctamente' });
+    } catch (error) {
+      console.log(error);
 
-
-      res.status(200).json({message: "Servicio editado correctamente"})
-    }catch( error) { 
-      console.log(error)
-
-      res.status(500).json({message:"Problema en editar servicio"})
-     
+      res.status(500).json({ message: 'Problema en editar servicio' });
     }
-  }
+  };
 
-  createService = async (req, res) =>{
+  createService = async (req, res) => {
     console.log(req.body.serviceData);
-    
+
     try {
       const data = {
         data: JSON.parse(req.body.serviceData),
-        img: req.file
-      }
-      await adminDal.createService(data)
-      res.status(200).json({message: "Servicio agregado correctamente"})
+        img: req.file,
+      };
+      await adminDal.createService(data);
+      res.status(200).json({ message: 'Servicio agregado correctamente' });
     } catch (error) {
-      res.status(500).json({message: "Ups!, error de inserción"})
-      console.log("eeeeeeeeeeee",error);
-      
-      
+      res.status(500).json({ message: 'Ups!, error de inserción' });
+      console.log('eeeeeeeeeeee', error);
     }
-  }
+  };
 
+  getUserList = async (req, res) => {
+    try {
+      const users = await adminDal.getUserList();
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ message: 'Error del servidor' });
+    }
+  };
+
+  disableUser = async (req, res) => {
+    const { userId } = req.params;
+    try {
+      await adminDal.disableUser(userId);
+    } catch (error) {
+      res.status(500).json({ message: 'Error en el back' });
+    }
+  };
 }
 
 export default new AdminControllers();
