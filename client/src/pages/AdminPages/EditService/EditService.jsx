079a5ff8201e21b735cf6beee5091885c4989065
edgar <edgararnauval@gmail.com>
2025-05-23 17:49:43 +0200

    Editar servicio comentado + maquetacion home

--- client/src/pages/AdminPages/EditService/EditService.jsx
+++ client/src/pages/AdminPages/EditService/EditService.jsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, {useEffect, useState } from 'react';
+/* import { fetchData } from '../../../helpers/axiosHelper.js';
+import {  useParams } from 'react-router-dom'; 
+ import { AuthContext } from '../../../helpers/axiosHelper.js'
+import { Button, Form } from 'react-bootstrap' */
+/* 
+const initialValue = {
+   name: "",
+   price: "",
+   description: "",
+   max_total: "",
+   image: "",
+} */
 
 const EditService = () => {
-  return (
-    <div>Edit Service
+  /* const { service_id } = useParams();
+  const [editService, setEditService] = useState();
+ const [service, setService, token] = useContext(AuthContext) 
+  const [message, setMessage] = useState("");
+  const [errorMsg, setErrorMsg] = useState("")
 
-      <h1>PAGINA PARA EDITAR LOS SERVICIOS POR ADMIN</h1>
-    </div>
+
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    const serviceToEdit = service.find((e) =>e.service_id === Number(service_id))
+    setEditService(serviceToEdit)
+
+  },[])
+
+
+
+  const handleChange = (e) =>{
+    const {name, value} = e.target;
+    setEditService({...editService,[name]:value})
+  }
+  
+
+  const onSubmit = async() => {
+    try {
+      let res = await fetchData('admin/editService', "PUT", editService, token)
+      console.log(res)
+
+      let newService = service.map((elem)=>{
+        if(elem.service_id === Number(service_id)){
+          return editService
+        }else{
+          return elem
+        }
+      })
+
+      setService(newService);
+      
+    } catch (error) {
+      setErrorMsg("Error en edicion")
+    }
+  }
+ */
+
+
+
+  return (
+    <div>h</div>
   );
 };
 
--- client/src/pages/PublicPages/Home/Home.jsx
+++ client/src/pages/PublicPages/Home/Home.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
   return (
     <div>
-      <h2>UWU</h2>
+      <section>
+        <div className="camper-nature">
+          <div className="camper-nature-text">
+            
+            <h1>CamperNature</h1>
+         
+            <h3>
+              "Tu espacio para viajar, descansar y conectar con la naturaleza"
+            </h3>
+            
+          </div>
+          <div className='camper-nature-boton'>
+            <button>RESERVA AHORA</button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
