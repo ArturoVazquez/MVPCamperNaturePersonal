a01e9933f02c403602233378b1be100d3c3273da
maria <kamina33@pm.me>
2025-06-05 21:09:33 +0200

    maquetacion reserva

--- client/src/pages/UserPages/Reserve/Reserve_4/Reserve_4.jsx
+++ client/src/pages/UserPages/Reserve/Reserve_4/Reserve_4.jsx
@@ -5,6 +5,7 @@ import { fetchData } from '../../../../helpers/axiosHelper';
 import { AuthContext } from '../../../../context/AuthContextProvider';
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
+import './reserve_4.css';
 
 const swalWithBootstrapButtons = Swal.mixin({
   customClass: {
@@ -49,21 +50,6 @@ export const Reserve_4 = ({
   }, []);
 
   console.log('Y AQUI LLEGA LA AMBULANCIAAAAAAAAAAAAAA', reservaData.startDate);
-
-  // const handleFinish = async () => {
-  //   try {
-  //     await fetchData(
-  //       'user/reserveDone',
-  //       'post',
-  //       { reservaData, price, parcelId, days },
-  //       token
-  //     );
-  //     setMessage('¡RESERVA EXITOSA!');
-  //   } catch (error) {
-  //     console.error('error del finish reserva', error);
-  //     throw error;
-  //   }
-  // };
 
   const handleFinish = async () => {
     const result = await swalWithBootstrapButtons.fire({
@@ -111,67 +97,95 @@ export const Reserve_4 = ({
   };
 
   return (
-    <section>
-      <Container>
-        <p className='mt-5 paso2'>Paso 4 de 4</p>
-        <h2 className="text-center fw-semibold title-service">Resumen de la Reserva</h2>
-        <Row>
-          <Col>
-            <article className='mt-4'>
-              <p className='title-p'><strong>Resumen de contacto</strong></p>
-              <p>
-                <strong>Nombre:</strong> {userDetails?.name}
+    <>
+      <section className='reserva4'>
+        <Container >
+          <p className="mt-5 paso2">Paso 4 de 4</p>
+          <h2 className="text-center fw-semibold title-service pb-4">
+            Resumen de la Reserva
+          </h2>
+          <Row className="reserve4-1">
+            <p className="title-p text-center">
+              <strong>Resumen de contacto</strong>
+            </p>
+            <Col className="col-12 col-lg-6">
+              <article className="mt-4">
+                <p>
+                  <strong>Nombre:</strong> {userDetails?.name}
+                </p>
+                <p>
+                  <strong>Apellidos:</strong> {userDetails?.lastname}
+                </p>
+                <p>
+                  <strong>Telefono de contacto:</strong> {userDetails?.prefix}{' '}
+                  {userDetails?.phone}
+                </p>
+                <p>
+                  <strong>Fecha de nacimiento:</strong>{' '}
+                  {userDetails?.birth_date}
+                </p>
+                <p>
+                  <strong>Dirección:</strong> {userDetails?.address}
+                </p>
+              </article>
+            </Col>
+            <Col className="col-12 col-lg-6">
+              <article className="mt-4">
+                <p>
+                  <strong>Documento:</strong> {userDetails?.document_type}{' '}
+                  {userDetails?.document_number}
+                </p>
+                <p>
+                  <strong>País:</strong> {userDetails?.country}
+                </p>
+                <p>
+                  <strong>Matrícula del coche:</strong>{' '}
+                  {userDetails?.car_registration}
+                </p>
+                <p>
+                  <strong>Modelo del coche:</strong> {userDetails?.car_brand}
+                </p>
+              </article>
+            </Col>
+          </Row>
+          <Row className="preferences my-4">
+            <div>
+              <p className='pt-2'>
+                <strong>Preferencias:</strong> 
               </p>
-              <p>
-                <strong>Apellidos:</strong> {userDetails?.lastname}
+              <p className='pb-2'>
+               {reservaData?.preferences}
               </p>
-              <p>
-                <strong>Telefono de contacto:</strong> {userDetails?.prefix}{' '}
-                {userDetails?.phone}
-              </p>
-              <p>
-                <strong>Fecha de nacimiento:</strong> {userDetails?.birth_date}
-              </p>
-              <p>
-                <strong>Dirección:</strong> {userDetails?.address}
-              </p>
-              <p>
-                <strong>Documento:</strong> {userDetails?.document_type}{' '}
-                {userDetails?.document_number}
-              </p>
-              <p>
-                <strong>País:</strong> {userDetails?.country}
-              </p>
-              <p>
-                <strong>Matrícula del coche:</strong>{' '}
-                {userDetails?.car_registration}
-              </p>
-              <p>
-                <strong>Modelo del coche:</strong> {userDetails?.car_brand}
-              </p>
-              <p>
-                <strong>Preferencias:</strong> {reservaData?.preferences}
-              </p>
-            </article>
-          </Col>
-          <Col>
-            <Row className="row-cols-1">
-              <Col>
-                <article className='mt-4'>
-                  <p className='title-p'><strong>Servicios Contratados</strong></p>
-                  {reservaData?.serviceNoIncluded.map((elem) => {
-                    return (
+            </div>
+          </Row>
+        </Container>
+      </section>
+      <section>
+        <Container >
+          <Row className="row-cols-1 row-cols-lg-2 reserve4-2">
+            <Col >
+              <article>
+                <p className="title-p">
+                  <strong>Servicios Contratados</strong>
+                </p>
+                {reservaData?.serviceNoIncluded.map((elem) => {
+                  return (
+                   
                       <p key={elem.service_id}>{`${elem.name}(${
                         elem.price
                       }€/día): ${elem.amount} x ${totalDays} días = ${
                         elem.amount * (totalDays * elem.price)
                       }€`}</p>
-                    );
-                  })}
-                </article>
-              </Col>
-              <Col>
-                <p>Reserva Confirmada:</p>
+                   
+                  );
+                })}
+              </article>
+            </Col>
+            <Col >
+              <article>
+                <p className="title-p">
+                  <strong>Reserva Confirmada</strong>
+                </p>
                 <p>
                   <strong>Check-in:</strong> {reservaData.startDate}
                 </p>
@@ -181,30 +195,36 @@ export const Reserve_4 = ({
                 <p>
                   <strong>Precio Total:</strong> {price}€
                 </p>
-              </Col>
-              <div className="d-flex justify-content-around pt-5">
-                <button
-                  type="button"
-                  className="botones-edit"
-                  onClick={() => setShowReserve(3)}
-                >
-                  Anterior
-                </button>
-                <button type="button" onClick={cancel} className="botones-edit">
-                  Cancelar
-                </button>
-                <button
-                  type="button"
-                  onClick={handleFinish}
-                  className="botones-edit"
-                >
-                  Confirmar
-                </button>
-              </div>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
-    </section>
+              </article>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section>
+        <Container>
+          <Row>
+            <div className="d-flex justify-content-around py-5">
+              <button
+                type="button"
+                className="botones-edit"
+                onClick={() => setShowReserve(3)}
+              >
+                Anterior
+              </button>
+              <button type="button" onClick={cancel} className="botones-edit">
+                Cancelar
+              </button>
+              <button
+                type="button"
+                onClick={handleFinish}
+                className="botones-edit"
+              >
+                Confirmar
+              </button>
+            </div>
+          </Row>
+        </Container>
+      </section>
+    </>
   );
 };
